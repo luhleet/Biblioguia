7dbea2a1ed95637fa9fe4c42281d70f228e43356
luhleet <74789293+luhleet@users.noreply.github.com>
2024-06-12 14:32:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-<body> <script type="module">
+<body> 
+  <script type="module">
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
