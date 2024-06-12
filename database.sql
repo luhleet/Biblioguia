617c7ace697b215ed20b15ac0524333062a3b8f5
luhleet <74789293+luhleet@users.noreply.github.com>
2024-06-12 13:52:13 -0300

    Update database

--- database.sql
+++ database.sql
@@ -1,1 +1,24 @@
-CREATE TABLE name ( id INT NOT NULL AUTO_INCREMENT , nome VARCHAR(50) NOT NULL, uf CHAR(2) NOT NULL, PRIMARY KEY (id))
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBh7Z_-N9GW4jzgY2KDB7W8kVWvEfzdyfk",
+    authDomain: "bilioguia.firebaseapp.com",
+    databaseURL: "https://bilioguia-default-rtdb.firebaseio.com",
+    projectId: "bilioguia",
+    storageBucket: "bilioguia.appspot.com",
+    messagingSenderId: "909471122953",
+    appId: "1:909471122953:web:a6be5a8646fc28134bdad8",
+    measurementId: "G-TG15WYXL61"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
