6e37395be34a082a885b61475ccde5ff4f9fbf28
luhleet <74789293+luhleet@users.noreply.github.com>
2024-07-24 21:38:59 -0300

    Delete script.sql

--- script.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATA  usuariosbiblioguia
-USE usuáriosbiblioguia
-CREATE TABLE usuários (
-  nome VARCHAR(60),
-  nascimento DATE
-  falecimento DATE
-  INSERT INTO usuários (Lena Vânia Ribeio Pinheiro,11/11/1941, 07/04/2024)
-  
-  
